54a1edb39a8cef3aee3eb4c8617ce06c25b53456
Gal Dagan <gald2345@gmail.com>
2024-02-06 19:12:29 +0200

    typo

--- src/features/expanses/expanses.routes-schema.ts
+++ src/features/expanses/expanses.routes-schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const baseExpnenseSchemaNoId = z.object({
+export const baseExpensesSchemaNoId = z.object({
   body: z.object({
     name: z.string().min(2, "Name must be at least 2 characters long").max(50, "Name must be no longer than 50 characters"),
     amount: z.number().positive("Amount must be greater than zero"),
