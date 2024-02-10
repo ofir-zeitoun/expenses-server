ea37ffacf4216a50815f7f41feb6e8f8e23a7848
Gal Dagan <gald2345@gmail.com>
2024-02-10 18:00:27 +0200

    add route for getbyid include the validation

--- src/features/expanses/expanses.routes.ts
+++ src/features/expanses/expanses.routes.ts
@@ -3,6 +3,7 @@ import status from "http-status";
 import { returnNew } from "../../db";
 import { validateResource } from "../../routes/middlewares";
 import { ExpansesModel } from "./expanses.model";
+import { expenseIdSchema } from "./expanses.routes-schema";
 
 export const router = Router();
 
@@ -11,57 +12,14 @@ router.get("/", async (_req, res) => {
   res.status(status.OK).json(items);
 });
 
-// router.get(
-//   "/:id",
-//   validateResource(singleTodoSchema),
-//   async (req: Request<SingleTodoInput["params"]>, res: Response) => {
-//     const item = await TodosModel.findById(req.params.id);
-//     res.status(status.OK).json(item);
-//   }
-// );
+router.get("/:_id", validateResource(expenseIdSchema), async (req: Request, res: Response) => {
+  const item = await ExpansesModel.findById(req.params._id);
 
-// router.post(
-//   "/",
-//   validateResource(createTodoSchema),
-//   async (req: Request<{}, {}, CreateTodoInput["body"]>, res: Response) => {
-//     // const added = new TodosModel(req.body);
-//     // added._id = new Types.ObjectId();
-//     // await added.save({ timestamps: true });
-//     const { insertedIds } = await TodosModel.insertMany(req.body, {
-//       rawResult: true,
-//     });
-//     const added = await TodosModel.findById(insertedIds[0]);
-//     res.status(status.OK).send(added);
-//   }
-// );
+  if (!item) {
+    return res.sendStatus(status.NOT_FOUND);
+  }
 
-// router.put(
-//   "/:id",
-//   validateResource(updateTodoSchema),
-//   async (
-//     req: Request<UpdateTodoInput["params"], {}, UpdateTodoInput["body"]>,
-//     res: Response
-//   ) => {
-//     const updated = await TodosModel.findByIdAndUpdate(
-//       req.params.id,
-//       req.body,
-//       returnNew
-//     );
-//     if (updated) {
-//       res.status(status.OK).send(updated);
-//     } else {
-//       res.sendStatus(status.NOT_FOUND);
-//     }
-//   }
-// );
-
-// router.delete(
-//   "/:id",
-//   validateResource(singleTodoSchema),
-//   async (req: Request<SingleTodoInput["params"]>, res: Response) => {
-//     const deleted = await TodosModel.findByIdAndRemove(req.params.id);
-//     res.sendStatus(deleted === null ? status.NOT_FOUND : status.OK);
-//   }
-// );
+  res.status(status.OK).json(item);
+});
 
 export default ["/api/expanses", router] as [string, Router];
