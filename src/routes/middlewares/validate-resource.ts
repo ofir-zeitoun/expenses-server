1ea580b3841a52d6f29a7f383e82347e9f41170e
Gal Dagan <gald2345@gmail.com>
2024-02-11 12:00:54 +0200

    reverted commit, according to pr

--- src/routes/middlewares/validate-resource.ts
+++ src/routes/middlewares/validate-resource.ts
@@ -2,24 +2,15 @@ import { NextFunction, Request, Response } from "express";
 import status from "http-status";
 import { AnyZodObject } from "zod";
 
-export const validateResource = (schema: AnyZodObject) => 
+export const validateResource =
+  (schema: AnyZodObject) =>
   (req: Request, res: Response, next: NextFunction) => {
     try {
-      switch (req.method) {
-        case 'POST':
-          schema.parse({ body: req.body });
-          break;
-        case 'GET':
-        case 'PUT':
-          schema.parse({
-            body: req.body,
-            query: req.query,
-            params: req.params,
-          });
-          break;
-        default:
-          throw new Error(`Unsupported request method: ${req.method}`);
-      }
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
       next();
     } catch (e: any) {
       return res.status(status.BAD_REQUEST).send(e.errors);
