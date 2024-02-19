2838ea99c4407167d85d658cdb14fb89b9370a91
bmalkes <bmalkes@gmail.com>
2024-02-19 20:25:16 +0200

    feat:add zod schema user

---
+++ src/features/users/user.routes-schema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const usersSchema = z.object({
+  body: z.object({
+    firstName: z
+      .string()
+      .min(2, "Name must be at least 2 characters long")
+      .max(50, "Name must be no longer than 50 characters"),
+    lastName: z
+      .string()
+      .min(2, "Name must be at least 2 characters long")
+      .max(50, "Name must be no longer than 50 characters"),
+    phone: z.number(),
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(2, "Password must be at least 2 characters long")
+      .max(50, "Password must be no longer than 24 characters"),
+    photo: z.string(),
+  }),
+});
