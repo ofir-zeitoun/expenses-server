0459617efc38ab8faaaec68d065b95e1e8a72468
Ofir Zeitoun <ofir-zeitoun@users.noreply.github.com>
2024-02-25 11:23:23 +0200

    #46 redesign model (#47)
    * remove ExpanseDocument
    use Expense in Schema
    
    * prettier

--- src/features/expenses/expenses.routes.ts
+++ src/features/expenses/expenses.routes.ts
@@ -3,7 +3,11 @@ import status from "http-status";
 import { ID, returnNew } from "../../db";
 import { validateResource } from "../../routes/middlewares";
 import { ExpansesModel, Expense } from "./expenses.model";
-import { baseExpensesSchemaNoId , expenseIdSchema , updateExpensesSchema } from "./expenses.routes-schema";
+import {
+  baseExpensesSchemaNoId,
+  expenseIdSchema,
+  updateExpensesSchema,
+} from "./expenses.routes-schema";
 
 export const router = Router();
 
@@ -21,37 +25,49 @@ router.post(
   }
 );
 
-router.get("/:id", validateResource(expenseIdSchema), async (req: Request<ID>, res: Response) => {
-  const item = await ExpansesModel.findById(req.params.id);
-  
-  if (!item) {
-    return res.sendStatus(status.NOT_FOUND);
-  }
-  
-  res.status(status.OK).json(item);
-});
+router.get(
+  "/:id",
+  validateResource(expenseIdSchema),
+  async (req: Request<ID>, res: Response) => {
+    const item = await ExpansesModel.findById(req.params.id);
 
-router.put("/:id", validateResource(updateExpensesSchema), async (req: Request<ID>, res: Response) => {
-  const updatedExpense = await ExpansesModel.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    returnNew
-    ); 
-    
+    if (!item) {
+      return res.sendStatus(status.NOT_FOUND);
+    }
+
+    res.status(status.OK).json(item);
+  }
+);
+
+router.put(
+  "/:id",
+  validateResource(updateExpensesSchema),
+  async (req: Request<ID>, res: Response) => {
+    const updatedExpense = await ExpansesModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      returnNew
+    );
+
     if (!updatedExpense) {
       return res.sendStatus(status.NOT_FOUND);
-  }
-  
-  res.status(status.OK).json(updatedExpense);
-});
+    }
 
-router.delete("/:id", validateResource(expenseIdSchema), async (req: Request<ID>, res: Response) => {
-  const deletedExpense = await ExpansesModel.findByIdAndDelete(req.params.id);
-
-  if (!deletedExpense) {
-    return res.sendStatus(status.NOT_FOUND);
+    res.status(status.OK).json(updatedExpense);
   }
-  res.status(status.OK).json(deletedExpense);
-});
+);
+
+router.delete(
+  "/:id",
+  validateResource(expenseIdSchema),
+  async (req: Request<ID>, res: Response) => {
+    const deletedExpense = await ExpansesModel.findByIdAndDelete(req.params.id);
+
+    if (!deletedExpense) {
+      return res.sendStatus(status.NOT_FOUND);
+    }
+    res.status(status.OK).json(deletedExpense);
+  }
+);
 
 export default ["/api/expenses", router] as [string, Router];
