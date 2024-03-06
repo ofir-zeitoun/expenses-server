83ab0ad252f0f18aa266daae09c55a8084c659d0
Gal Dagan <gald2345@gmail.com>
2024-03-06 17:23:27 +0200

    I made corrections according to the review

--- src/features/authentication/authentication.routes.ts
+++ src/features/authentication/authentication.routes.ts
@@ -1,10 +1,7 @@
 import { Router, Request, Response } from "express";
 import { checkJwt } from "../../routes/middlewares/auth0";
-import status from "http-status";
-
 
 const router = Router();
-router.use("/",checkJwt, (req, res) => {
-});
+router.use("/", checkJwt, (req, res) => {});
 
 export default ["/", router] as [string, Router];
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,14 @@
 import { Express } from "express";
 import "express-yields";
-import { healthCheck } from "../features/health-check";
+import authentication from "../features/authentication";
 import expenses from "../features/expenses";
-import  authentication  from "../features/authentication";
+import { healthCheck } from "../features/health-check";
 import { errorHandler, catchAllRequestsLastRouteHandler } from "./middlewares";
-
 
 export const routes = (app: Express) => {
   app.get(...healthCheck);
   app.use(...authentication);
   app.use(...expenses);
 
-  // add custom error handler middleware as the last middleware
   app.use(catchAllRequestsLastRouteHandler, errorHandler);
 };
