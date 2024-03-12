a87af1e736f2486b49eef0918a383b2a09ec7957
bmalkes <bmalkes@gmail.com>
2024-03-12 20:03:49 +0200

    fix:zod schema...

--- src/features/users/user.routes-schema.ts
+++ src/features/users/user.routes-schema.ts
@@ -10,11 +10,11 @@ export const usersSchema = z.object({
       .string()
       .min(2, "Name must be at least 2 characters long")
       .max(50, "Name must be no longer than 50 characters"),
-    phone: z.number(),
+    phone: z.string(),
     email: z.string().email(),
     password: z
       .string()
-      .min(2, "Password must be at least 2 characters long")
+      .min(8, "Password must be at least 8 characters long")
       .max(50, "Password must be no longer than 24 characters"),
     photo: z.string(),
   }),
