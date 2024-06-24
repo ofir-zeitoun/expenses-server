63d3e84b234896a8a745cb3ed92af515ca1bbffd
Shaked Levy <shakedlv0@gmail.com>
2024-06-24 16:25:36 +0300

    Removed duplicated health-check.ts

--- src/features/health-check.ts
+++
@@ -1,9 +0,0 @@
-import { Request, RequestHandler, Response } from "express";
-import status from "http-status";
-
-export const healthCheck: [string, RequestHandler] = [
-  "/health-check",
-  (_req: Request, res: Response) => {
-    res.sendStatus(status.OK);
-  },
-];
